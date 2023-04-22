7a87647ff85fdc3509ecfd7c6c5fd947001e9310
ezhoon <dlwlgns1240@gmail.com>
2023-04-22 16:21:40 +0900

    [code-labs-basics] rememberSaveable

--- code-labs-basic/src/main/java/com/example/code_labs_basic/MainActivity.kt
+++ code-labs-basic/src/main/java/com/example/code_labs_basic/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,7 +44,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by remember {
+    var shouldShowOnboarding by rememberSaveable {
         mutableStateOf(true)
     }
 
