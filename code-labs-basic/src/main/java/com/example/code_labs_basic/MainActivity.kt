6f6dd8c0a54daaedad207f4f5e2ddd3e4d6ca5c7
ezhoon <dlwlgns1240@gmail.com>
2023-04-22 16:13:31 +0900

    [code-labs-basics] State hoisting

--- code-labs-basic/src/main/java/com/example/code_labs_basic/MainActivity.kt
+++ code-labs-basic/src/main/java/com/example/code_labs_basic/MainActivity.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.Dimension
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -27,7 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposestudyTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    MyApp()
+                    MyApp(Modifier.fillMaxSize())
                 }
             }
         }
@@ -35,7 +40,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(
+private fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember {
+        mutableStateOf(true)
+    }
+
+    Surface(modifier = modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
@@ -49,8 +69,7 @@ private fun MyApp(
 }
 
 @Composable
-fun Greeting(name: String) {
-
+private fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
 
     Surface(
@@ -83,17 +102,47 @@ fun Greeting(name: String) {
     }
 }
 
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the basics codeLab")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = {
+                onContinueClicked.invoke()
+            }
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
 private fun extraPaddingBottomWhenExpanded(
     expanded: Boolean,
     @Dimension(unit = Dimension.DP)
-    dp : Dp
+    dp: Dp
 ) = if (expanded) dp else 0.dp
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    ComposestudyTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposestudyTheme {
-        MyApp()
+        Greetings()
     }
 }
 
@@ -102,5 +151,13 @@ fun DefaultPreview() {
 fun GreetingPreview() {
     ComposestudyTheme {
         Greeting("Android")
+    }
+}
+
+@Preview
+@Composable
+fun OnboardingPreview() {
+    ComposestudyTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
