1a87323b58f2824c80be49266bf069761c391c07
ezhoon <dlwlgns1240@gmail.com>
2023-06-02 21:38:07 +0900

    [practice] constraintlayout set

--- practice/src/main/java/com/example/practice/fast/ConstraintLayout.kt
+++ practice/src/main/java/com/example/practice/fast/ConstraintLayout.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import com.example.practice.ui.theme.ComposestudyTheme
 
 @Composable
@@ -61,9 +63,79 @@ fun EzConstraintLayout() {
 }
 
 @Composable
+fun EzConstraintSet() {
+
+    val constraintSet = ConstraintSet {
+        val redBox = createRefFor("redBox")
+        val magentaBox = createRefFor("magentaBox")
+        val greenBox = createRefFor("greenBox")
+        val yellowBox = createRefFor("yellowBox")
+
+        constrain(redBox) {
+            bottom.linkTo(parent.bottom, margin = 8.dp)
+            end.linkTo(parent.end, margin = 4.dp)
+        }
+
+        constrain(magentaBox) {
+            start.linkTo(parent.start, margin = 8.dp)
+            end.linkTo(parent.end, margin = 4.dp)
+        }
+
+        constrain(greenBox) {
+            start.linkTo(magentaBox.start, margin = 8.dp)
+            bottom.linkTo(magentaBox.bottom, margin = 8.dp)
+        }
+
+        constrain(yellowBox) {
+            centerTo(parent)
+        }
+    }
+
+    ConstraintLayout(
+        constraintSet = constraintSet,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Red)
+                .layoutId("redBox")
+        )
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Magenta)
+                .layoutId("magentaBox")
+        )
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Green)
+                .layoutId("greenBox")
+        )
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Yellow)
+                .layoutId("yellowBox")
+        )
+    }
+
+}
+
+@Composable
 @Preview
 fun ExConstraintLayoutPreview() {
     ComposestudyTheme {
         EzConstraintLayout()
+    }
+}
+
+@Composable
+@Preview
+fun EzConstraintSetPreview() {
+    ComposestudyTheme {
+        EzConstraintSet()
     }
 }
